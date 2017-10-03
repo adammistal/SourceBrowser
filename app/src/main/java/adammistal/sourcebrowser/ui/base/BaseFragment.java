09f045ac32e07936b39971fbf69d5c115b4c3b55
Adam Mistal <a.mistal@madiff.pl>
2017-10-03 17:00:43 +0200

    Add configchanges

--- app/src/main/java/adammistal/sourcebrowser/ui/base/BaseFragment.java
+++ app/src/main/java/adammistal/sourcebrowser/ui/base/BaseFragment.java
@@ -1,6 +1,5 @@
 package adammistal.sourcebrowser.ui.base;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
